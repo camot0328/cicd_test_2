c867666790d4228b9285a1bda4cb2c21ee900ca3
camot0328 <camot0328@gmail.com>
2025-06-24 11:36:32 +0900

    add testcode

---
+++ src/test/java/org/example/cicdtest2/controller/MainControllerTest.java
@@ -0,0 +1,16 @@
+package org.example.cicdtest2.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MainControllerTest {
+
+  @Test
+  void main() {
+    Assertions.assertThat(true).isTrue();
+  }
+}
